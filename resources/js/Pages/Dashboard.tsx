462d8e216519663c033bca94955afbc09c041748
Johcel Gene T. Bitara • BS IT <genebitara@gmail.com>
2025-08-12 23:59:48 +0800

    fix: removed stray character.

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -15,7 +15,7 @@ export default function Dashboard({ auth }: PageProps) {
           </h2>
         }
       >
-        <Head title="Dashboard" />s
+        <Head title="Dashboard" />
         <div className="py-12">
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
