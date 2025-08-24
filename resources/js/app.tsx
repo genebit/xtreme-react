679b7266c84ae7d54be21647693e24cbe019308b
Johcel Gene T. Bitara • BS IT <genebitara@gmail.com>
2025-08-24 13:15:53 +0800

    chore: added bs5 js bundle.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,6 @@
 import "./bootstrap";
 import "../css/app.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
