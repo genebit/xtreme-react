b8c2d1605b42942e38a60dfb83540c38b622f96a
Johcel Gene T. Bitara • BS IT <genebitara@gmail.com>
2025-08-30 04:05:48 +0800

    chore: added pace-js pckg.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,7 @@
 import "./bootstrap";
 import "../css/app.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "pace-js";
 
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
