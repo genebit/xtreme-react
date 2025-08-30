173fcb0ee7ef955fb523813b8be3edf564fd6c09
Johcel Gene T. Bitara • BS IT <genebitara@gmail.com>
2025-08-31 00:56:30 +0800

    chore: added datatables.net pckg.

--- resources/js/Components/Preloader.tsx
+++ resources/js/Components/Preloader.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Preloader() {
   const [isLoading, setIsLoading] = useState(true);
+  const [hidePreloader, setHidePreloader] = useState(false);
 
   useEffect(() => {
     const handleLoad = () => setIsLoading(false);
@@ -14,10 +15,21 @@ export default function Preloader() {
     }
   }, []);
 
+  useEffect(() => {
+    if (!isLoading) {
+      const timer = setTimeout(() => setHidePreloader(true), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [isLoading]);
+
   return (
     <div
       className={`animate__animated tw-fixed top-0 left-0 tw-w-screen tw-h-screen overflow-hidden tw-bg-white tw-transition-opacity tw-duration-700 tw-flex tw-items-center tw-justify-center ${
-        isLoading ? "tw-opacity-100 tw-z-50" : "animate__fadeOut tw-z-40"
+        isLoading
+          ? "tw-opacity-100 tw-z-50"
+          : !isLoading && !hidePreloader
+          ? "animate__fadeOut tw-z-40"
+          : "tw-z-0 tw-opacity-0"
       }`}
     >
       <span className="preloader"></span>
